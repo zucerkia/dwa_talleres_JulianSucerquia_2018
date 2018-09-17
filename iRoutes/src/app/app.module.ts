ae0eab6d0719db279217fa7f68890077393795da
zucerkia <32274355+zucerkia@users.noreply.github.com>
2018-09-17 15:53:38 -0500

    geolocalization was used to center the map

--- iRoutes/src/app/app.module.ts
+++ iRoutes/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
 
 import { AgmCoreModule } from '@agm/core';
+// import { environment } from '../environments/environment.prod';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,7 @@ import { AgmCoreModule } from '@agm/core';
   imports: [
     BrowserModule,
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyBZAztkvcmi-DUE6JXHWFOhJibh2KSaHw0'
+      apiKey: environment.googleMapsKey
     })
   ],
   providers: [],
--- iRoutes/src/app/components/map/map.component.ts
+++ iRoutes/src/app/components/map/map.component.ts
@@ -5,14 +5,24 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
+
 export class MapComponent implements OnInit {
 
-  lat: number = 51.678418;
-  lng: number = 7.809007;
-  
+  lat: number;
+  lng: number;
+  zoom:number = 16;
+
   constructor() { }
 
   ngOnInit() {
+    this.getUserLocation()
   }
-
+  private getUserLocation(){
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(pos=>{
+        this.lat = pos.coords.latitude;
+        this.lng = pos.coords.longitude;
+      })
+    }
+  }
 }
