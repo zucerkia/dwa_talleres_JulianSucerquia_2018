bfe5899c65a83852acea0168e51c2355cf040611
zucerkia <32274355+zucerkia@users.noreply.github.com>
2018-09-17 14:45:45 -0500

    google maps was added

--- iRoutes/src/app/app.module.ts
+++ iRoutes/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './components/map/map.component';
+
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBZAztkvcmi-DUE6JXHWFOhJibh2KSaHw0'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
