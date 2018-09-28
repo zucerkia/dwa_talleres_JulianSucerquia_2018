80be4a3817b0a1a3b5d74e2c4fbb758ef03918b6
zucerkia <32274355+zucerkia@users.noreply.github.com>
2018-09-28 07:41:42 -0500

    initial components were created

--- angular-pokedex/src/app/app.module.ts
+++ angular-pokedex/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PokemainComponent } from './pages/pokemain/pokemain.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemainComponent,
+    HeaderComponent,
+    FooterComponent,
+    PokemonCardComponent
   ],
   imports: [
     BrowserModule
---
+++ angular-pokedex/src/app/pages/pokemain/pokemain.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemain',
+  templateUrl: './pokemain.component.html',
+  styleUrls: ['./pokemain.component.scss']
+})
+export class PokemainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
