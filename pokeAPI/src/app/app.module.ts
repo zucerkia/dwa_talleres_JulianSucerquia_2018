01a3352ad28fcdbd79f3db67a4606d7032817d87
zucerkia <32274355+zucerkia@users.noreply.github.com>
2018-10-29 14:54:40 -0500

    main page was created

--- pokeAPI/src/app/app.module.ts
+++ pokeAPI/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
+import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
